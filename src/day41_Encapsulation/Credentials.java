e6b72c54d43464f3e3b3c0d1ec723d7bc193c159
erenkucuk <erenkkucuk89@gmail.com>
2022-07-07 19:46:38 -1000

    added new class

--- src/day41_Encapsulation/Credentials.java
+++ src/day41_Encapsulation/Credentials.java
@@ -2,4 +2,30 @@ package day41_Encapsulation;
 
 public class Credentials {
 
+    private String userName = "erenk";
+    private double password = 12345;
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public double getPassword(){
+        return password;
+    }
+
+    public void setUserName(String userName){
+        /*
+        if (userName.length()<8){
+            return;
+        }
+        we can also do security check in setter of Encapsulation
+
+         */
+        this.userName = userName;
+    }
+
+    public void setPassword(double password){
+        this.password = password;
+    }
+
 }

53de12017c96e13ad1bf8d8f69784980c69e5e10
erenkucuk <erenkkucuk89@gmail.com>
2022-07-26 13:42:52 -1000

    added new class

---
+++ src/day46_JavaRecap/ShapeTask/ShapeObjects.java
@@ -0,0 +1,13 @@
+package day46_JavaRecap.ShapeTask;
+
+public class ShapeObjects {
+
+    public static void main(String[] args) {
+
+        Circle circle = new Circle(-1);
+
+        System.out.println(circle);
+        System.out.println(circle.getR());
+        System.out.println(circle.getD());
+    }
+}

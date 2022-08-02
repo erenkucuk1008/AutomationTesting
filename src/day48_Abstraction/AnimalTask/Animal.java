90710be79d8304d7727b40754984fda7cb14bc52
erenkucuk <erenkkucuk89@gmail.com>
2022-08-02 13:44:23 -1000

    added new class

---
+++ src/day48_Abstraction/AnimalTask/Animal.java
@@ -0,0 +1,27 @@
+package day48_Abstraction.AnimalTask;
+
+public abstract class Animal {
+
+    public String name, breed;
+    public String gender;
+    public int age;
+
+    public Animal(String name, String breed, String gender, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public abstract void sleep();
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}

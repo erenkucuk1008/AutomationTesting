7b377139ef4a987a767c988bab1d1a9142868e32
erenkucuk <erenkkucuk89@gmail.com>
2022-08-01 22:39:20 -1000

    added new class

--- src/day44_Inheritance/PetTask/Dog.java
+++ src/day44_Inheritance/PetTask/Dog.java
@@ -5,4 +5,15 @@ public class Dog extends Pet {
     public Dog(String name, String breed, char gender, int age, String color) {
         super(name, breed, gender, age, color);
     }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                ", gender=" + gender +
+                ", age=" + age +
+                ", color='" + color + '\'' +
+                '}';
+    }
 }

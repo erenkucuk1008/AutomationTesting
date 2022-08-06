625e4eec20ff7950308b64397221940fbb1b43b2
erenkucuk <erenkkucuk89@gmail.com>
2022-08-06 01:28:41 -1000

    added new class

--- src/day52_Collection/SetPractice.java
+++ src/day52_Collection/SetPractice.java
@@ -9,15 +9,15 @@ public class SetPractice { //we just store unique datas
     public static void main(String[] args) {
 
         Set<String> names1 = new HashSet<>();//randomly print out
-        names1.addAll(Arrays.asList("Eren","Arzum","Eren","Eren","Hakan","Suleyman","Eren","Sevgi"));
+        names1.addAll(Arrays.asList("Eren", "Arzum", "Eren", "Eren", "Hakan", "Suleyman", "Eren", "Sevgi"));
         System.out.println(names1);
 
         Set<String> names2 = new LinkedHashSet<>();//ne yazdiysak o
-        names2.addAll(Arrays.asList("Eren","Arzum","Eren","Eren","Hakan","Suleyman","Eren","Sevgi"));
+        names2.addAll(Arrays.asList("Eren", "Arzum", "Eren", "Eren", "Hakan", "Suleyman", "Eren", "Sevgi"));
         System.out.println(names2);
 
         Set<String> names3 = new TreeSet<>();//sorted print out
-        names3.addAll(Arrays.asList("Eren","Arzum","Eren","Eren","Hakan","Eren","Suleyman","Sevgi"));
+        names3.addAll(Arrays.asList("Eren", "Arzum", "Eren", "Eren", "Hakan", "Eren", "Suleyman", "Sevgi"));
         System.out.println(names3);
 
         System.out.println("===================================");
@@ -25,12 +25,12 @@ public class SetPractice { //we just store unique datas
         //remove duplicates
         String str = "gggggaaaabbbbccddddddddddddddddeefff";
 
-        String result= "";//1.YOL icin
-        String result1="";//3.YOL icin
+        String result = "";//1.YOL icin
+        String result1 = "";//3.YOL icin
 
         //1.YOL
-        for (String s : new LinkedHashSet<>(Arrays.asList(str.split("")))){
-            result+= s;
+        for (String s : new LinkedHashSet<>(Arrays.asList(str.split("")))) {
+            result += s;
         }
         System.out.println(result);//String olarak yazdirdik
 
@@ -53,7 +53,7 @@ public class SetPractice { //we just store unique datas
         hs.addAll(Arrays.asList(arr1));
 
         for (String each : hs) {
-            result1+=each;
+            result1 += each;
         }
         System.out.println(result1);//String olarak yazdirdik. For each loop kullanarak
 
@@ -84,17 +84,17 @@ public class SetPractice { //we just store unique datas
         //System.out.println(str3.toUpperCase());
 
         HashSet<Integer> hashSet = new HashSet<>();
-        hashSet.addAll(Arrays.asList(null,null,null,null,null));
+        hashSet.addAll(Arrays.asList(null, null, null, null, null));
         System.out.println(hashSet);
 
         LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<>();
-        linkedHashSet.addAll(Arrays.asList(null,null,null,null,null));
+        linkedHashSet.addAll(Arrays.asList(null, null, null, null, null));
         System.out.println(linkedHashSet);
 
         System.out.println("===================================");
 
         Set<Integer> numbers = new HashSet<>();
-        numbers.addAll(Arrays.asList(1,2,3,4,5,6,8,9,10));
+        numbers.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 8, 9, 10));
 
         int maxNum = Collections.max(numbers);
         System.out.println("maxNum = " + maxNum);
@@ -106,18 +106,18 @@ public class SetPractice { //we just store unique datas
 
         //HATIRLATMA---SWAP 1 VE 3. INDEXIN YERI DEGISTIR
         List<Integer> scores = new ArrayList<>();
-        scores.addAll(Arrays.asList(1,2,3,4,5));
+        scores.addAll(Arrays.asList(1, 2, 3, 4, 5));
 
-        Collections.swap(scores,1,3);
+        Collections.swap(scores, 1, 3);
         System.out.println(scores);
 
         System.out.println("========================");
 
         String eren = "dddaaaaccbb";//d3a4c2b2
 
-        LinkedHashSet<String> e1 = new LinkedHashSet<>(Arrays.asList(eren.split("")));
-        for (String each : e1) {
-            System.out.print(each + Collections.frequency(Arrays.asList(eren.split("")),each));
+        LinkedHashSet<String> deneme = new LinkedHashSet<>(Arrays.asList(eren.split("")));
+        for (String each : deneme) {
+            System.out.print(each + Collections.frequency(Arrays.asList(eren.split("")), each));
         }
     }
 }

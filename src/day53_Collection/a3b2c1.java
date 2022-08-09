f2b6f9c718b2fb493231d0f5b39682c8e522e99e
erenkucuk <erenkkucuk89@gmail.com>
2022-08-09 00:17:42 -1000

    added new class

---
+++ src/day53_Collection/a3b2c1.java
@@ -0,0 +1,16 @@
+package day53_Collection;
+
+import java.util.*;
+
+public class a3b2c1 {
+
+    public static void main(String[] args) {
+
+        String str= "aaabbc";
+
+        Set<String> s = new LinkedHashSet<>(Arrays.asList(str.split("")));
+        for (String each : s) {
+            System.out.print(each+Collections.frequency(Arrays.asList(str.split("")),each));
+        }
+    }
+}

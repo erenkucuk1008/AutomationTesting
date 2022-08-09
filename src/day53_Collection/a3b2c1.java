9637416a24ee0a607c0bdd4edbded6c7f7615dd8
erenkucuk <erenkkucuk89@gmail.com>
2022-08-09 00:29:22 -1000

    added new class

--- src/day53_Collection/a3b2c1.java
+++ src/day53_Collection/a3b2c1.java
@@ -9,6 +9,7 @@ public class a3b2c1 {
         String str= "aaabbc";
 
         Set<String> s = new LinkedHashSet<>(Arrays.asList(str.split("")));
+
         for (String each : s) {
             System.out.print(each+Collections.frequency(Arrays.asList(str.split("")),each));
         }

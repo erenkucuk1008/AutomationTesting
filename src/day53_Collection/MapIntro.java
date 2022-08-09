0cf9b4468c22c2087578e712b401b683c879d2b8
erenkucuk <erenkkucuk89@gmail.com>
2022-08-09 00:05:40 -1000

    added new class

--- src/day53_Collection/MapIntro.java
+++ src/day53_Collection/MapIntro.java
@@ -1,4 +1,14 @@
 package day53_Collection;
 
+import java.util.*;
+
 public class MapIntro {
+
+    Map<String, String> credentials = new HashMap<>();// random, accepts null
+
+    Map<String, String> credentials2 = new LinkedHashMap<>();// keeps the insertion order, accepts null
+
+    Map<String, String> credentials3 = new TreeMap<>();// sorted, does not accept null
+
+    Map<String, String> credentials4 = new Hashtable<>();//is synchronized, thread-safe
 }

8e4811de8588100355db61ebe3707428687f7d1d
erenkucuk <erenkkucuk89@gmail.com>
2022-08-10 01:45:07 -1000

    added new class

--- src/day54_Map/Browsers.java
+++ src/day54_Map/Browsers.java
@@ -4,3 +4,19 @@ public enum Browsers {
 
     Chrome, Firefox, Safari
 }
+
+enum MonthName{
+    Jan, Feb, Mar, Apr, May, Jun, July, Aug, Sep, Oct, Nov, Dec
+}
+
+enum DaysInWeek{
+    Mon, Tue, Wed, Thu, Fri, Sat, Sun
+}
+
+enum ComputerOS{
+    MacOS, Windows, Linux
+}
+
+enum Priority{
+    Low, Medium, High
+}
--- src/day54_Map/TestEnum.java
+++ src/day54_Map/TestEnum.java
@@ -22,5 +22,27 @@ public class TestEnum {
                 System.out.println("Safari is selected");
         }
 
+        String monthName = "Python";
+        MonthName monthName1 = MonthName.Jun;
+
+        String dayInWeek = "Eren";
+        DaysInWeek day = DaysInWeek.Fri;
+
+        ComputerOS os = ComputerOS.Windows;
+
+        Priority priorityLevel = Priority.High;
+
+        System.out.println("===========================================");
+        String string = "Cybertek";
+        string.concat(" School"); // "Cybertek School"
+        System.out.println(string);  // "Cybertek"
+
+        StringBuilder stringBuilder = new StringBuilder("Cybertek");
+        stringBuilder.append(" School");
+        stringBuilder.reverse();
+        System.out.println(stringBuilder);
+
+
+
     }
 }
